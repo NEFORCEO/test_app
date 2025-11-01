b04bf0dbd1f663916eb0ca61613d8ceca31658fa
NEFORCEO <pirogartem919@gmail.com>
2025-11-01 20:11:01 +0700

    New_routers

--- routers/router.py
+++ routers/router.py
@@ -11,22 +11,61 @@ router = APIRouter(
     tags=config.tags
 )
 
-@router.get("/get_devs")
-async def get_devs(db: SessionDep) -> list[Dev]:
+@router.get("/get_devs", response_model=list[Dev])
+async def get_devs(db: SessionDep):
     get_devs = await db.execute(select(Devs))
     return get_devs.scalars().all()
 
-@router.post('/add_devs')
-async def post_devs(devs: Add_devs, db: SessionDep) -> Dev:
-    if_valid_dev = await db.execute(select(Devs).filter(Dev.name == devs.name))
-    valid_dev = if_valid_dev.scalars().first()
+@router.get('/get_devs/{id}', response_model=Dev)
+async def get_devs_id(id: int, db: SessionDep):
+    result = await db.execute(select(Devs).filter(Devs.dev_id == id))
+    id_devs = result.scalars().first()
+    
+    if not id_devs:
+        return Response(
+            status_code=404,
+            content="Такого пользователя не существует"
+        )
+    return Dev(
+        dev_id=id_devs.dev_id,
+        name=id_devs.name
+    )
 
-    if valid_dev:
+@router.post("/add_devs", response_model=Dev)
+async def post_devs(devs: Add_devs, db: SessionDep):
+    if_devs = await db.execute(select(Devs).filter(Devs.name == devs.name))
+    valid_devs = if_devs.scalars().first()
+    
+    if valid_devs:
         return Response(
             status_code=409,
-            content="Пользователь уже существует"
+            content="Такой пользователь уже существует"
         )
     new_devs = Devs(name=devs.name)
     db.add(new_devs)
     await db.commit()
     await db.refresh(new_devs)
+    return Dev(
+        dev_id=devs.dev_id,
+        name=devs.name
+    )    
+    
+@router.delete("/delete_devs/{id}", response_model=Dev)
+async def delete_devs(id: int, db: SessionDep):
+    result = await db.execute(select(Devs).filter(Devs.dev_id == id))
+    delete_user = result.scalars().first()
+    
+    if not delete_user:
+        return Response(
+            status_code=404,
+            content="Такого пользователя не существует"
+        )
+        
+    await db.delete(delete_user)
+    await db.commit()
+    
+    return Dev(
+        dev_id=delete_user.dev_id,
+        name = delete_user.name
+    )
+    
